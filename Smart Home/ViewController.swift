513c8fbef6963ccfb94c2c16172994a13926e6fc
Miroslaw Hudaszek <miroslaw.hudaszek@miquido.com>
2021-03-02 08:31:06 +0100

    Initial project

---
+++ Smart Home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Home
+//
+//  Created by Mirosław Hudaszek on 01/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
